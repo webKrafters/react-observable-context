26b6a17c1434ded1c6959f5cd49d2e085a67d3c6
Stephen Isienyi <stephen.isienyi@gmail.com>
2024-02-19 09:24:51 -0800

    iphone viewport fix and client route rendering

--- docs/app/root.tsx
+++ docs/app/root.tsx
@@ -38,7 +38,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#1890ff"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16"/>
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32"/>
         <link rel="icon" href="/favicon-192x192.png" sizes="192x192"/>
