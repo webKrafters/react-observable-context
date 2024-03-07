7ed99df7569d083415bd54fdcf7e1d6ba49a12bf
Stephen Isienyi <stephen.isienyi@gmail.com>
2024-03-07 13:59:28 -0800

    integrate webkrafters universal-storage for user settings

--- docs/app/util/universal-storage.ts
+++ docs/app/util/universal-storage.ts
@@ -1,9 +1,14 @@
-import { clientOnly$, serverOnly$ } from 'vite-env-only';
+import {
+    getClientStorage,
+    getServerStorage
+} from 'universal-storage';
+
+import {
+    clientOnly$,
+    serverOnly$
+} from 'vite-env-only';
 
 import type { RootContext } from '~/root';
-
-import jsCookie from 'js-cookie';
-import { DARKMODE_LOCALSTORAGE_KEY } from '~/constants';
 
 enum ENVIRONMENT {
     CLIENT = 'Client',
@@ -20,44 +25,19 @@ interface DualStorage<E extends ENVIRONMENT = ENVIRONMENT.SERVER> {
 
 let storage : DualStorage<ENVIRONMENT>;
 
+clientOnly$((() => { storage = getClientStorage().current })());
+
 serverOnly$((() => {
+    const serverStorage = getServerStorage();
     storage = {
         getItem: ( key : string, rootContext? : RootContext ) => (
-            rootContext?.cookies?.[ DARKMODE_LOCALSTORAGE_KEY.toLowerCase() ] as string|void
+            serverStorage.current.getItem( key.toLowerCase(), rootContext )
         ),
         removeItem: ( key : string ) => {
             throw new Error( 'Cannot remove a serer response cookie from here.' );
         },
         setItem: <T = any>( key : string, value : T ) => {
-             throw new Error( 'Cannot set a serer response cookie from here.' );   
-        }
-    }
-})());
-
-clientOnly$((() => {
-    storage = {
-        getItem: ( key : string ) => {
-            let val = jsCookie.get( key ) as string;
-            if( typeof val !== 'undefined' ) {
-                window.localStorage && setTimeout(
-                    () => window.localStorage.setItem( key , val ),
-                    0
-                );
-                return val;
-            }
-            val = window?.localStorage?.getItem( key ) as string;
-            if( typeof val === 'undefined' ) { return 'false' };
-            setTimeout( () => jsCookie.set( key, val ), 0 );
-            return val;
-        },
-        removeItem: ( key : string ) => {
-            jsCookie.remove( key, { path: '/' } );
-            window?.localStorage?.removeItem( key );
-        },
-        setItem: <T = any>( key : string, value : T ) => {
-            const val = `${ value }`;
-            jsCookie.set( key, val, { path: '/' } );
-            window?.localStorage?.setItem( key, val );
+            throw new Error( 'Cannot set a serer response cookie from here.' );   
         }
     }
 })());
