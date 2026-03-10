97af6cfaefe4ffe3051a937d4a0b031d3a94fb9f
webKrafter <54419534+webKrafter@users.noreply.github.com>
2026-03-10 11:51:01 -0700

    Update provider.tsx
    Signed-off-by: webKrafter <54419534+webKrafter@users.noreply.github.com>

--- src/pages/concepts/provider.tsx
+++ src/pages/concepts/provider.tsx
@@ -19,7 +19,7 @@ const ConceptProviderPage : React.FC<{className? : string}> = ({ className }) =>
                    <div>
                         <h3>What is the Provider?</h3>
                         <div>
-                            <Paragraph>The Provider component is a property of the Eagle Eye context object. As a <code>React.Context</code> API based provider, it accepts the customary <code>children</code> and <code>value</code> props. It also accepts <strong>2</strong> optional props: <Anchor to="/concepts/prehooks"><code>prehooks</code></Anchor> and <Anchor to="/concepts/storage"><code>storage</code></Anchor>.</Paragraph>
+                            <Paragraph>The Provider component is a property of the React Observable Context object. As a <code>React.Context</code> API based provider, it accepts the customary <code>children</code> and <code>value</code> props. It also accepts <strong>2</strong> optional props: <Anchor to="/concepts/prehooks"><code>prehooks</code></Anchor> and <Anchor to="/concepts/storage"><code>storage</code></Anchor>.</Paragraph>
                             <Paragraph>External direct access to the context store may be obtained via the <code>ref</code> attribute. Please see a <Anchor to="/getting-started#provider-usage">Provider Usage</Anchor> sample.</Paragraph>
                             <Paragraph>Routinely, the <code>value</code>  prop is initialized with the full initial state. It may only be updated with parts of the state which are changing. Please see a <Anchor to="/getting-started#provider-usage">Provider Usage</Anchor> sample.</Paragraph>
                         </div>
